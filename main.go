7d7b6549c29594ae66fdab3f767c3c85b5ed03b3
mrkresnofatih <mrkresnofatih@gmail.com>
2023-03-21 08:05:37 +0700

    tried to generate tfdocs

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"github.com/mrkresnofatih/terraform-provider-xata/xata"
 )
 
+// Generate the Terraform provider documentation using `tfplugindocs`:
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
